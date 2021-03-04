e53527ab8df90a97e5b684f82af7e12423a92314
William Kennedy <bill@ardanlabs.com>
2021-03-04 17:58:32 -0500

    class2 commit2

--- app/sales-api/main.go
+++ app/sales-api/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"expvar"           // Register the vars handler
+	_ "net/http/pprof" // Register the pprof handler
+
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"time"
 
@@ -34,7 +38,7 @@ func run(log *log.Logger) error {
 			DebugHost       string        `conf:"default:0.0.0.0:4000"`
 			ReadTimeout     time.Duration `conf:"default:5s"`
 			WriteTimeout    time.Duration `conf:"default:5s"`
-			ShutdownTimeout time.Duration `conf:"default:5s"`
+			ShutdownTimeout time.Duration `conf:"default:5s,noprint"`
 		}
 	}
 	cfg.Version.SVN = build
@@ -59,6 +63,38 @@ func run(log *log.Logger) error {
 		}
 		return errors.Wrap(err, "parsing config")
 	}
+
+	// =========================================================================
+	// App Starting
+
+	expvar.NewString("build").Set(build)
+	log.Printf("main: Started: Application initializing: version %q", build)
+	defer log.Println("main: Completed")
+
+	out, err := conf.String(&cfg)
+	if err != nil {
+		return errors.Wrap(err, "generating config for output")
+	}
+	log.Printf("main: Config:\n%v\n", out)
+
+	// =========================================================================
+	// Start Debug Service
+	//
+	// /debug/pprof - Added to the default mux by importing the net/http/pprof package.
+	// /debug/vars - Added to the default mux by importing the expvar package.
+	//
+	// Not concerned with shutting this down when the application is shutdown.
+
+	log.Println("main: Initializing debugging support")
+
+	go func() {
+		log.Printf("main: Debug Listening %s", cfg.Web.DebugHost)
+		if err := http.ListenAndServe(cfg.Web.DebugHost, http.DefaultServeMux); err != nil {
+			log.Printf("main: Debug Listener closed: %v", err)
+		}
+	}()
+
+	select {}
 
 	return nil
 }
